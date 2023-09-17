da2af0b24d8766662d78a8b0c1220b51c7aedd2d
omarbdw <omarbdw@yahoo.com>
2023-09-17 01:31:22 -0700

    startingheader files to inculde and define

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
+#include <stdlib.h>
+#define OUTPUT_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define NULL_STRING "(null)"
+#define PARAMS_INIT {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+#define CONVERT_LOWER_CASE 1
+#define CONVERT_UNSIGNED 2
